60cc6fd293ea0866eddad16542e5eeb2807d3f8c
Do Anh Vu <vufabio@gmail.com>
2023-09-16 00:08:31 +0700

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sql = require('mssql');
 
 const app = express();
+const port = 3000;
 
 // Cấu hình kết nối đến cơ sở dữ liệu SQL Server
 const config = {
@@ -10,36 +11,36 @@ const config = {
   server: 'vufabio_db.mssql.somee.com', // Thay đổi server và database của bạn
   database: 'vufabio_db',
   options: {
+    enableArithAbort: true,
     encrypt: true, // Sử dụng mã hóa (encrypt) nếu cần thiết
     trustServerCertificate: true
   }
 };
 
-// Kết nối đến cơ sở dữ liệu SQL Server
-sql.connect(config, err => {
-  if (err) {
-    console.error('Lỗi kết nối đến cơ sở dữ liệu: ' + err.message);
-  } else {
-    console.log('Đã kết nối đến cơ sở dữ liệu');
-  }
-});
+// Kết nối đến SQL Server
+sql.connect(config)
+  .then(() => {
+    console.log('Đã kết nối đến SQL Server');
+  })
+  .catch((err) => {
+    console.error('Lỗi kết nối đến SQL Server:', err);
+  });
 
 // Định nghĩa route để lấy danh sách từ bảng "Region"
 app.get('/regions', (req, res) => {
-  const query = 'SELECT * FROM Region';
+  const request = new sql.Request();
 
-  sql.query(query, (err, result) => {
-    if (err) {
-      console.error('Lỗi truy vấn cơ sở dữ liệu: ' + err.message);
-      res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
-    } else {
+  request.query('SELECT * FROM Region')
+    .then((result) => {
       res.json(result.recordset);
-    }
-  });
+    })
+    .catch((err) => {
+      console.error('Lỗi truy vấn cơ sở dữ liệu:', err);
+      res.status(500).json({ error: 'Lỗi truy vấn cơ sở dữ liệu' });
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server đang lắng nghe tại cổng ${PORT}`);
+// Khởi động máy chủ
+app.listen(port, () => {
+  console.log(`Máy chủ đang lắng nghe tại cổng ${port}`);
 });
